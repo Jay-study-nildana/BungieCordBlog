29316ec516b48ea2e4bf487c23604ad8fb4a1fee
Jay <vijayasimhabr@live.com>
2025-08-03 13:25:14 +0530

    added bungie hello world api
    for some time now I have been thinking of adding a hello world project
    that will act as a stepping stone for the main bungie project
    
    this is that stepping stone

---
+++ BungieHelloWorldAPI/HelloWorldAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HelloWorldAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloWorldAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+    }
+}
